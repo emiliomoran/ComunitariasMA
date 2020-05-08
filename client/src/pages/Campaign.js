d6cd69761c864e150c1231fbd602bab3e5d3ea2e
emiliomoran <emiliojmp9@gmail.com>
2020-05-07 22:12:53 -0500

    Merge

--- client/src/pages/Campaign.js
+++ client/src/pages/Campaign.js
@@ -4,192 +4,206 @@ import { Row } from "antd";
 import Api from "../utils/Api";
 
 class Campaign extends React.Component {
-	constructor(props) {
-	    super(props);
-	    this.state = {
-	      	data: [],
-	      	loading: true,
-	    };
-  	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: true,
+    };
+  }
 
-  	componentDidMount = () => {
-    	this.getCampaign();
-  	};
+  componentDidMount = () => {
+    this.getCampaign();
+  };
 
-  	getCampaign = () => {
-  		Api.get('campaign/').then((response) => {
-  			let data = [];
-  			response.data.map((item) => {
-  				let campaign = {
-  					key: item.id,
-  					name: item.name,
-  					description: item.description,
-  					contactName: item.contactName,
+  getCampaign = () => {
+    Api.get("campaign/")
+      .then((response) => {
+        let data = [];
+        response.data.map((item) => {
+          let campaign = {
+            key: item.id,
+            name: item.name,
+            description: item.description,
+            contactName: item.contactName,
             photo: item.photo,
-  				};
-  				data.push(campaign);
-  				return true;
-  			});
-  			this.setState({
-  				data: data,
-  				loading: false,
-  			});
-  		}).catch((error) => {
-  			this.setState({
-  				loading: false,
-  			});
-  			console.log(error);
-  		});
-  	};
+          };
+          data.push(campaign);
+          return true;
+        });
+        this.setState({
+          data: data,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.log(error);
+      });
+  };
 
-  	addCampaign = (data) => {
-  		this.setState({
-  			loading: true,
-  		});
-  		Api.post('campaign/', {
-  			name: data.name,
-  			contactName: data.contactName,
-		    description: data.description,
-		    photo: data.photo,
-		    createdBy: "reactclient",
-  		}).then((response) => {
-  			console.log(response);
-        	this.getCampaign();
-  		}).catch((error) => {
-  			this.setState({
-  				loading: false,
-  			});
-  			console.log(error);
-  		});
-  	};
+  addCampaign = (data) => {
+    this.setState({
+      loading: true,
+    });
+    console.log(data);
+    let formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("contactName", data.contactName);
+    formData.append("description", data.description);
+    formData.append("photo", data.photo);
+    formData.append("createdBy", "reactclient");
+    Api.post("campaign/", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        this.getCampaign();
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.log(error);
+      });
+  };
 
-  	editCampaign = (data) => {
-  		this.setState({
-  			loading: true,
-  		});
-  		Api.put(`campaign/${data.key}/`, {
-  			name: data.name,
-  			contactName: data.contactName,
-		    description: data.description,
-		    photo: data.photo,
-		    createdBy: "reactclient",
-  		}).then((response) => {
-  			console.log(response);
-        	this.getCampaign();
-  		}).catch((error) => {
-  			this.setState({
-  				loading: false,
-  			});
-  			console.log(error);
-  		});
-  	};
+  editCampaign = (data) => {
+    this.setState({
+      loading: true,
+    });
+    Api.put(`campaign/${data.key}/`, {
+      name: data.name,
+      contactName: data.contactName,
+      description: data.description,
+      photo: data.photo,
+      createdBy: "reactclient",
+    })
+      .then((response) => {
+        console.log(response);
+        this.getCampaign();
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.log(error);
+      });
+  };
 
-  	deleteCampaign = (data) => {
-  		this.setState({
-  			loading: true,
-  		});
-  		Api.delete(`campaign/${data.key}/`)
-  		.then((response) => {
-  			console.log(response);
-        	this.getCampaign();
-  		}).catch((error) => {
-  			this.setState({
-  				loading: false,
-  			});
-  			console.log(error);
-  		});
-  	};
+  deleteCampaign = (data) => {
+    this.setState({
+      loading: true,
+    });
+    Api.delete(`campaign/${data.key}/`)
+      .then((response) => {
+        console.log(response);
+        this.getCampaign();
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.log(error);
+      });
+  };
 
-  	deleteLogicCampaign = (data) => {
-  		this.setState({
-  			loading: true,
-  		});
-  		Api.put(`campaign/${data.key}/`, {
-  			state: 0,
-  		}).then((response) => {
-  			console.log(response);
-        	this.getCampaign();
-  		}).catch((error) => {
-  			this.setState({
-  				loading: false,
-  			});
-  			console.log(error);
-  		});
-  	};
+  deleteLogicCampaign = (data) => {
+    this.setState({
+      loading: true,
+    });
+    Api.put(`campaign/${data.key}/`, {
+      state: 0,
+    })
+      .then((response) => {
+        console.log(response);
+        this.getCampaign();
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+        });
+        console.log(error);
+      });
+  };
 
-  	render() {
-  		const { data, loading } = this.state;
+  render() {
+    const { data, loading } = this.state;
 
-  		const columns = [
-  			{
-  				title: "Nombre",
-  				key: "name",
-  			},
-  			{
-  				title: "Descripción",
-  				key: "description",
-  			},
-  			{
-  				title: "Contacto",
-  				key: "contactName",
-  			},
-  			{
-  				title: "Acción",
-  				key: "action",
-  			},
-        {
-          title: "Foto",
-          key: "photo",
-        },
-  		];
+    const columns = [
+      {
+        title: "Nombre",
+        key: "name",
+      },
+      {
+        title: "Descripción",
+        key: "description",
+      },
+      {
+        title: "Contacto",
+        key: "contactName",
+      },
+      {
+        title: "Acción",
+        key: "action",
+      },
+      {
+        title: "Foto",
+        key: "photo",
+      },
+    ];
 
-  		const fieldsForm = [
-  			{
-		        key: "name",
-		        label: "Nombre de Campaña",
-		        required: true,
-		        maxLength: 50,
-		        type: "text",
-      		},
-      		{
-		        key: "contactName",
-		        label: "Contacto",
-		        required: true,
-		        maxLength: 50,
-		        type: "text",
-      		},
-      		{
-		        key: "description",
-		        label: "Descripción",
-		        required: true,
-		        maxLength: 50,
-		        type: "textArea",
-      		},
-      		{
-		        key: "photo",
-		        label: "Foto de Campaña",
-		        required: false,
-		        type: "file",
-      		},
-  		];
+    const fieldsForm = [
+      {
+        key: "name",
+        label: "Nombre de Campaña",
+        required: true,
+        maxLength: 50,
+        type: "text",
+      },
+      {
+        key: "contactName",
+        label: "Contacto",
+        required: true,
+        maxLength: 50,
+        type: "text",
+      },
+      {
+        key: "description",
+        label: "Descripción",
+        required: true,
+        maxLength: 50,
+        type: "textArea",
+      },
+      {
+        key: "photo",
+        label: "Foto de Campaña",
+        required: false,
+        type: "file",
+      },
+    ];
 
-  		return (
-  			<Row>
-  				<h3>Campañas</h3>
-  				<CrudTable
-  					columns={columns}
-			        data={data}
-			        fieldsForm={fieldsForm}
-			        title="Campaña"
-			        add={this.addCampaign}
-			        edit={this.editCampaign}
-          		delete={this.deleteCampaign}
-			        loading={loading}
-			        includesMap={false}
-  				/>
-  			</Row>
-  		);
-  	}
+    return (
+      <Row>
+        <h3>Campañas</h3>
+        <CrudTable
+          columns={columns}
+          data={data}
+          fieldsForm={fieldsForm}
+          title="Campaña"
+          add={this.addCampaign}
+          edit={this.editCampaign}
+          delete={this.deleteCampaign}
+          loading={loading}
+          includesMap={false}
+        />
+      </Row>
+    );
+  }
 }
 
 export default Campaign;
